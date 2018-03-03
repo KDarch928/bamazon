c5662c985ce32e65d9acb1be50c0784c796afe03
KDarch928 <Kayle.Drucker@gmail.com>
2018-03-03 17:41:50 -0600

    create the product pruchase and add items to the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,161 +16,162 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+//make connection to mysql, then run the function to start the program.
 connection.connect(function(err) {
     if (err) throw err;
-    startProgram();
+    runProgram();
 });
 
-function startProgram() {
+function runProgram() {
+    console.log("Welcome to Bamazon!");
     displayInventory();
+    setTimeout(start, 1000);
+    //start()
 }
 
-// function displayInventory() {
-//     connection.query("SELECT * from product", function (err, res) {
-//         if(err) throw err;
-//         console.table(res);
-//     });
-//     connection.end();
-// }
-
+//function to display the items to the user
 function displayInventory() {
     connection.query("SELECT item_id as ID, product_name as Product, department_name as Department, price as Price, stock_quanity as Stock from product", function (err, res) {
         if(err) throw err;
         console.table(res);
     });
+}
+
+//function to end the program
+function endProgram() {
     connection.end();
 }
 
+// function which prompts the user for what action they should take
+function start() {
+    inquirer
+        .prompt({
+            name: "buyOrSell",
+            type: "rawlist",
+            message: "Would you like to [BUY] an item or [SELL] an item or [QUIT]?",
+            choices: ["BUY", "SELL","QUIT"]
+        })
+        .then(function(answer) {
+            // based on their answer, either call the buy or the sell or quit functions
+            if (answer.buyOrSell === "BUY") {
+                purchaseItem();
+            } else if (answer.buyOrSell === "QUIT"){
+                endProgram();
+            } else {
+                sellItem();
+            }
+        });
+}
 
-// function runSearch() {
-//     inquirer
-//         .prompt({
-//             name: "action",
-//             type: "list",
-//             message: "What would you like to do?",
-//             choices: [
-//                 "Find songs by artist",
-//                 "Find all artists who appear more than once",
-//                 "Find data within a specific range",
-//                 "Search for a specific song"
-//             ]
-//         })
-//         .then(function(answer) {
-//             switch (answer.action) {
-//                 case "Find songs by artist":
-//                     artistSearch();
-//                     break;
-//
-//                 case "Find all artists who appear more than once":
-//                     multiSearch();
-//                     break;
-//
-//                 case "Find data within a specific range":
-//                     rangeSearch();
-//                     break;
-//
-//                 case "Search for a specific song":
-//                     songSearch();
-//                     break;
-//             }
-//         });
-// }
+function purchaseItem() {
 
-// function artistSearch() {
-//     inquirer
-//         .prompt({
-//             name: "artist",
-//             type: "input",
-//             message: "What artist would you like to search for?"
-//         })
-//         .then(function(answer) {
-//             var query = "SELECT position, song, year FROM top5000 WHERE ?";
-//             connection.query(query, { artist: answer.artist }, function(err, res) {
-//                 for (var i = 0; i < res.length; i++) {
-//                     console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-//                 }
-//                 runSearch();
-//             });
-//         });
-// }
+    // query the database for all items being auctioned
+    connection.query("SELECT * FROM product", function(err, results) {
+        if (err) throw err;
+        // once you have the items, prompt the user for which they would like to buy
+        inquirer
+            .prompt([
+                {
+                    name: "itemID",
+                    type: "input",
+                    message: "Enter the ID of the Item you would like to buy?"
+                },
+                {
+                    name: "qty",
+                    type: "input",
+                    message: "How many would you like to buy?"
+                }
+            ])
+            .then(function(answer) {
+                // get the information of the item selected
+                var selection;
 
-// function multiSearch() {
-//     var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-//     connection.query(query, function(err, res) {
-//         for (var i = 0; i < res.length; i++) {
-//             console.log(res[i].artist);
-//         }
-//         runSearch();
-//     });
-// }
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id === parseFloat(answer.itemID)) {
+                        selection = results[i];
+                    }
+                }
 
-// function rangeSearch() {
-//     inquirer
-//         .prompt([
-//             {
-//                 name: "start",
-//                 type: "input",
-//                 message: "Enter starting position: ",
-//                 validate: function(value) {
-//                     if (isNaN(value) === false) {
-//                         return true;
-//                     }
-//                     return false;
-//                 }
-//             },
-//             {
-//                 name: "end",
-//                 type: "input",
-//                 message: "Enter ending position: ",
-//                 validate: function(value) {
-//                     if (isNaN(value) === false) {
-//                         return true;
-//                     }
-//                     return false;
-//                 }
-//             }
-//         ])
-//         .then(function(answer) {
-//             var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-//             connection.query(query, [answer.start, answer.end], function(err, res) {
-//                 for (var i = 0; i < res.length; i++) {
-//                     console.log(
-//                         "Position: " +
-//                         res[i].position +
-//                         " || Song: " +
-//                         res[i].song +
-//                         " || Artist: " +
-//                         res[i].artist +
-//                         " || Year: " +
-//                         res[i].year
-//                     );
-//                 }
-//                 runSearch();
-//             });
-//         });
-// }
+                // //check to see if the there is enough quality to satisfy their purchase.
+                if (selection.stock_quanity > 0 && selection.stock_quanity > answer.qty){
+                    connection.query(
+                        "UPDATE product SET ? WHERE ?",
+                        [
+                            {
+                                stock_quanity: (selection.stock_quanity - answer.qty)
+                            },
+                            {
+                                item_id: selection.item_id
+                            }
+                        ],
+                        function(err){
+                            if (err) throw err;
+                            console.log("You have successfully purhcased a " + selection.product_name + " for $" + selection.price);
+                            runProgram();
+                        }
+                    );
+                } else {
+                    console.log("Not enough quantity to complete your purchased!!")
+                    runProgram();
+                }
+            });
+    });
+}
 
-// function songSearch() {
-//     inquirer
-//         .prompt({
-//             name: "song",
-//             type: "input",
-//             message: "What song would you like to look for?"
-//         })
-//         .then(function(answer) {
-//             console.log(answer.song);
-//             connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-//                 console.log(
-//                     "Position: " +
-//                     res[0].position +
-//                     " || Song: " +
-//                     res[0].song +
-//                     " || Artist: " +
-//                     res[0].artist +
-//                     " || Year: " +
-//                     res[0].year
-//                 );
-//                 runSearch();
-//             });
-//         });
-// }
+// function for a customers to sell an item
+function sellItem() {
+    // prompt for info about the item being sold
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "input",
+                message: "What is the item you would like to submit?"
+            },
+            {
+                name: "dept",
+                type: "input",
+                message: "What department would this item go in?"
+            },
+            {
+                name: "itemPirce",
+                type: "input",
+                message: "How much would you like to sell the item?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "itemQty",
+                type: "input",
+                message: "How many of them item are you selling?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function(answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                "INSERT INTO product SET ?",
+                {
+                    product_name: answer.item,
+                    department_name: answer.dept,
+                    price: answer.itemPrice,
+                    stock_quanity: answer.itemQty
+                },
+                function(err) {
+                    if (err) throw err;
+                    console.log("Your " + answer.item + " was created successfully!");
+                    // re-prompt the user for if they want to buy or sell another item
+                    runProgram();
+                }
+            );
+        });
+}
