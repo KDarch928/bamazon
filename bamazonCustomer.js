9dee46552439f17a83443019de0dadaa86f67859
KDarch928 <Kayle.Drucker@gmail.com>
2018-03-03 14:06:49 -0600

    Update code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
 var mysql = require("mysql");
+require("dotenv").config();
 var inquirer = require("inquirer");
+var table = require('console.table');
+var key = require("./key.js");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,56 +12,69 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
-    password: "",
+    password: key.mysqlaccess.access,
     database: "bamazon"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    startProgram();
 });
 
 function startProgram() {
-    
+    displayInventory();
 }
+
+// function displayInventory() {
+//     connection.query("SELECT * from product", function (err, res) {
+//         if(err) throw err;
+//         console.table(res);
+//     });
+//     connection.end();
+// }
 
 function displayInventory() {
-    
+    connection.query("SELECT item_id as ID, product_name as Product, department_name as Department, price as Price, stock_quanity as Stock from product", function (err, res) {
+        if(err) throw err;
+        console.table(res);
+    });
+    connection.end();
 }
 
-function runSearch() {
-    inquirer
-        .prompt({
-            name: "action",
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-                "Find songs by artist",
-                "Find all artists who appear more than once",
-                "Find data within a specific range",
-                "Search for a specific song"
-            ]
-        })
-        .then(function(answer) {
-            switch (answer.action) {
-                case "Find songs by artist":
-                    artistSearch();
-                    break;
 
-                case "Find all artists who appear more than once":
-                    multiSearch();
-                    break;
-
-                case "Find data within a specific range":
-                    rangeSearch();
-                    break;
-
-                case "Search for a specific song":
-                    songSearch();
-                    break;
-            }
-        });
-}
+// function runSearch() {
+//     inquirer
+//         .prompt({
+//             name: "action",
+//             type: "list",
+//             message: "What would you like to do?",
+//             choices: [
+//                 "Find songs by artist",
+//                 "Find all artists who appear more than once",
+//                 "Find data within a specific range",
+//                 "Search for a specific song"
+//             ]
+//         })
+//         .then(function(answer) {
+//             switch (answer.action) {
+//                 case "Find songs by artist":
+//                     artistSearch();
+//                     break;
+//
+//                 case "Find all artists who appear more than once":
+//                     multiSearch();
+//                     break;
+//
+//                 case "Find data within a specific range":
+//                     rangeSearch();
+//                     break;
+//
+//                 case "Search for a specific song":
+//                     songSearch();
+//                     break;
+//             }
+//         });
+// }
 
 // function artistSearch() {
 //     inquirer
